e8a8df329dfe8afe8296a36c64742828a723937f
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2022-10-03 09:41:20 +0200

    Add project files.

---
+++ Fish.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA221003
+{
+    internal class Fish
+    {
+        private float _weight;
+        private bool _weightIsSet = false;
+        private bool _predator;
+        private int _swimTop;
+        private int _swimDepth;
+        private string _species;
+
+        public float Weight
+        { 
+            get => _weight;
+            set
+            {
+                if (value < .5f)
+                    throw new Exception("a hal súlya túl alacsony");
+                if (value > 40f)
+                    throw new Exception("a hal súlya túl nagy");
+                if (_weightIsSet)
+                {
+                    if (value < _weight * .9f)
+                        throw new Exception("a hal súlya ennyivel nem csökkenhet egyszerrre");
+                    if (value > _weight * 1.1f)
+                        throw new Exception("a hal súlya ennyivel nem növekedhet egyszerre");
+                }
+                _weight = value;
+            }
+        }
+        public bool Predator 
+        {
+            get => _predator;
+            set => _predator = value;
+        }
+        public int SwimTop
+        { 
+            get => _swimTop;
+            set => _swimTop = value;
+        }
+        public int SwimDepth 
+        { 
+            get => _swimDepth;
+            set => _swimDepth = value;
+        }
+        public string Species 
+        { 
+            get => _species;
+            set => _species = value;
+        }
+
+        public Fish(float weight, bool predator, int swimTop, int swimDepth, string species)
+        {
+            Weight = weight;
+            _weightIsSet = true;
+            Predator = predator;
+            SwimTop = swimTop;
+            SwimDepth = swimDepth;
+            Species = species;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CA221003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fish fish = new(
+                weight: 25F,
+                predator: false,
+                swimTop: 0,
+                swimDepth: 0,
+                species: null);
+
+            Console.WriteLine(
+                "A hal adatai:\n" +
+                $"\tSúly: {fish.Weight} Kg");
+
+            fish.Weight -= 2.5F;
+
+
+            Console.WriteLine(
+                "A hal adatai:\n" +
+                $"\tSúly: {fish.Weight} Kg");
+        }
+    }
+}
